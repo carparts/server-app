3d869bc4cf542b426299c7cb0d6d4717f7201cda
Ego <lalala@fakemail.kom>
2016-09-23 02:51:15 +0300

    Express js added

--- main/app.js
+++ main/app.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log('Hello Amigos');
+
+const express = require('express');
+const server = express();
+const port = 9999;
+
+server.get('/', (request, response) => {
+    response.send('Hello from Express.js!')
+});
+
+
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+});
